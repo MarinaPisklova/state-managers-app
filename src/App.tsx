acafda7a3119608b078b7c3457497c1db6081185
Pisklova Marina <pisklovama@gmail.com>
2024-12-06 16:55:12 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import TodoInput from './ToDo/TodoInput';
+import TodoList from './ToDo/TodoList';
+import styles from './App.module.css';
+import { observer, useLocalObservable } from 'mobx-react-lite';
+import { useStore } from './stores';
+import { useEffect } from 'react';
+import { when } from 'mobx';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const { todos } = useStore();
 
-export default App;
+    useEffect(() => {
+        const promiseWhen = when(() => !appUI.todosVisible);
+
+        promiseWhen.then(() => {
+            console.log('clean up!');
+        });
+    }, []);
+
+    const appUI = useLocalObservable(() => ({
+        todosVisible: true,
+        loading: false,
+        toggleTodoVisibility() {
+            this.todosVisible = !this.todosVisible;
+        },
+    }));
+
+    return (
+        <div className="app">
+            <TodoInput />
+            <div className={styles['todo-list-wrapper']}>
+                <h2 onClick={appUI.toggleTodoVisibility}>
+                    <span>{appUI.todosVisible ? '-' : '+'}</span>
+                    Todos (unfinished {todos.unfinishedTodos.length})
+                </h2>
+                {appUI.todosVisible && <TodoList />}
+            </div>
+        </div>
+    );
+};
+
+export default observer(App);
---
+++ src/ToDo/TodoInput/index.tsx
@@ -0,0 +1,26 @@
+import { FormEvent } from 'react';
+import styles from './TodoInput.module.css';
+import { useStore } from '../../stores';
+
+const TodoInput = () => {
+    const { todos } = useStore();
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault();
+
+        const formElement = e.target as HTMLFormElement;
+        const formData = new FormData(formElement);
+        const value = String(formData.get('todo-input') || '');
+
+        todos.add(value);
+        formElement.reset();
+    };
+
+    return (
+        <form onSubmit={handleSubmit} className={styles['todo-input-group']}>
+            <input name="todo-input" placeholder="Add todo..." />
+            <button type="submit">Add Todo</button>
+        </form>
+    );
+};
+
+export default TodoInput;
